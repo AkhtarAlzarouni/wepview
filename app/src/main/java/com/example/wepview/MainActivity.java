8a269f3b5a4dad4292ed751d3b052f10fd67ee98
Akhtar Alzrouni <138444303+AkhtarAlzarouni@users.noreply.github.com>
2023-08-17 12:36:49 +0400

    Initial commit

---
+++ app/src/main/java/com/example/wepview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.wepview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSdBkYfwUJcm4lg9596C2IhAIEm0UvUUwTQSk0khSz6wivFnZQ/viewform");
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
